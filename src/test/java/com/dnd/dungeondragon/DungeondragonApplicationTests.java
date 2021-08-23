eb8b02280cb02ebb1ee54aba00a5e15b9c7c4034
Steven GROBE <steven.grobe@Digital-Grenoble.local>
2021-08-23 11:47:38 +0200

    Inital commit

---
+++ src/test/java/com/dnd/dungeondragon/DungeondragonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.dungeondragon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeondragonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
